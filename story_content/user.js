480b9a80f702a205721093011ee13e7f3eee3d11
Mohammad Qashoa <mohammad.m.q@gmail.com>
2017-08-10 11:41:45 +0300

    update site links

--- story_content/user.js
+++ story_content/user.js
@@ -1,1 +1,17 @@
-// User defined code goes here
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6QZ0ANR17p7":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
